8025952b1f4a143bdf1a7716b02123dbc775e78a
KrispyKreme01 <krmalatrash@gmail.com>
2017-05-14 18:57:45 -0400

    *fixed winCheck by adding brackets around the && statements *fixed tieCheck by changing it to a counter

--- src/Testo.java
+++ src/Testo.java
@@ -13,6 +13,7 @@ public class Testo {
 	public static Scanner sc = new Scanner(System.in);
 	public static int i = 1;
 	public static boolean turn = false;
+	public static int counter = 0;
 	public static void main(String[] args) {
 		for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -20,19 +21,23 @@ public class Testo {
             }
         }
 		wlcmMSG();
-		while(!winCheck() || !tieCheck()) {
+		while(!winCheck() && !tieCheck()) {
 			crdPCK();
 			i++;
+			counter++;
 		}
 		if (tieCheck()) {
+			brdPRNT();
 			System.out.println("issa tie");
 			System.exit(1);
 		}
 		else if (turn) {
+			brdPRNT();
 			System.out.println("X WINS");
 			System.exit(1);
 		}
 		else {
+			brdPRNT();
 			System.out.println("O WINS");
 			System.exit(1);
 		}
@@ -111,27 +116,27 @@ public class Testo {
 					if (tttbrd[0][0]== 'X' && tttbrd[0][1]== 'X' && tttbrd[0][2] =='X' || tttbrd[0][0]== 'O' && tttbrd[0][1]== 'O' && tttbrd[0][2] =='O') {
 						return true;
 					}
-					else if (tttbrd[1][0]== 'X' && tttbrd[1][1]== 'X' && tttbrd[1][2] == 'X' || tttbrd[1][0]== 'O' && tttbrd[1][1]== 'O' && tttbrd[1][2] == 'O') {
+					else if ((tttbrd[1][0]== 'X' && tttbrd[1][1]== 'X' && tttbrd[1][2] == 'X') || (tttbrd[1][0]== 'O' && tttbrd[1][1]== 'O' && tttbrd[1][2] == 'O')) {
 						return true;
 					}
-					else if (tttbrd[2][0]== 'X' && tttbrd[2][1]== 'X' && tttbrd[2][2] == 'X' || tttbrd[2][0]== 'O' && tttbrd[2][1]== 'O' && tttbrd[2][2] == 'O') {
+					else if ((tttbrd[2][0]== 'X' && tttbrd[2][1]== 'X' && tttbrd[2][2] == 'X') || (tttbrd[2][0]== 'O' && tttbrd[2][1]== 'O' && tttbrd[2][2] == 'O')) {
 						return true;
 					}
 				//checks rows
-					else if (tttbrd[0][0]== 'X' && tttbrd[1][0]== 'X' && tttbrd[2][0] =='X' || tttbrd[0][0]== 'O' && tttbrd[1][0]== 'O' && tttbrd[2][0] =='O') {
+					else if ((tttbrd[0][0]== 'X' && tttbrd[1][0]== 'X' && tttbrd[2][0] =='X') || (tttbrd[0][0]== 'O' && tttbrd[1][0]== 'O' && tttbrd[2][0] =='O')) {
 						return true;
 					}
-					else if (tttbrd[0][1]== 'X' && tttbrd[1][1]== 'X' && tttbrd[2][1] =='X' || tttbrd[0][1]== 'O' && tttbrd[1][1]== 'O' && tttbrd[2][1] =='O') {
+					else if ((tttbrd[0][1]== 'X' && tttbrd[1][1]== 'X' && tttbrd[2][1] =='X') || (tttbrd[0][1]== 'O' && tttbrd[1][1]== 'O' && tttbrd[2][1] =='O')) {
 						return true;				
 					}
-					else if (tttbrd[0][2]== 'X' && tttbrd[1][2]== 'X' && tttbrd[2][2] =='X' || tttbrd[0][2]== 'O' && tttbrd[1][2]== 'O' && tttbrd[2][2] =='O') {
+					else if ((tttbrd[0][2]== 'X' && tttbrd[1][2]== 'X' && tttbrd[2][2] =='X') || (tttbrd[0][2]== 'O' && tttbrd[1][2]== 'O' && tttbrd[2][2] =='O')) {
 						return true;
 					}
 				//checks diagonal and anti-diagonal
-					else if (tttbrd[0][0]== 'X' && tttbrd[1][1]== 'X' && tttbrd[2][2] =='X' || tttbrd[0][0]== 'O' && tttbrd[1][1]== 'O' && tttbrd[2][2] =='O') {
+					else if ((tttbrd[0][0]== 'X' && tttbrd[1][1]== 'X' && tttbrd[2][2] =='X') || (tttbrd[0][0]== 'O' && tttbrd[1][1]== 'O' && tttbrd[2][2] =='O')) {
 						return true;
 					}
-					else if (tttbrd[0][2]== 'X' && tttbrd[1][1]== 'X' && tttbrd[2][0] =='X' || tttbrd[0][2]== 'O' && tttbrd[1][1]== 'O' && tttbrd[2][0] =='O') {
+					else if ((tttbrd[0][2]== 'X' && tttbrd[1][1]== 'X' && tttbrd[2][0] =='X') || (tttbrd[0][2]== 'O' && tttbrd[1][1]== 'O' && tttbrd[2][0] =='O')) {
 						return true;
 					}
 					else {
@@ -141,19 +146,12 @@ public class Testo {
 			//needs to be implemented, this method will check to see if someone won
 		}
 		public static boolean tieCheck() {
-			boolean flag = false;
-			for (int l = 0; l < 3; l++) {
-				for (int k = 0; k < 3; k++ ) {
-					if (tttbrd[l][k] == 'X' || tttbrd[l][k] == 'O' ) {
-						flag = true;
-					}
-				}
-			}
-			if (flag) { 
-				return false;
-		}
-			else{
+			if (counter >= 9) {
 				return true;
 			}
+			else {
+				return false;
+			}
+
 		}
 }
